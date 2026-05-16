0caec5c0d310a7d45f9f349f4f180cd9fad68ff9
MD Ratul <tahid7266@gmail.com>
2026-05-17 02:25:40 +0600

    setup environment and file management

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -20,9 +24,10 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${poppins.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}</body>
     </html>
   );
 }
---
+++ src/components/AnimalsCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnimalsCard = () => {
+    return (
+        <div>
+            animals card
+        </div>
+    );
+};
+
+export default AnimalsCard;
